bfe6eaf1fb7b2bc5a6cd216f3062c5525e34eb8a
Lee-Juntong <2310266421@qq.com>
2020-04-01 02:03:37 +0800

    allows user to enter the path directly. the output is still rgb 565 format, which later version would allow different settings depending on the user input

--- bmp2coe.c
+++ bmp2coe.c
@@ -3,15 +3,15 @@
 //and the output file can be used directly by verilog and output to a 96*64 oled screen
 //as I disabled some information text 
 
-#define _CRT_SECURE_NO_DEPRECATE
+
 #include <windows.h> 
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<string.h>
 FILE *outfp;
-int carry2;
+
 int BMPtoCOE(char *BMPfilename, int isfinished)
-{
+{   int carry2;
     FILE *fp;
     if ((fp = fopen(BMPfilename, "rb")) == NULL)
     {
@@ -21,35 +21,34 @@ int BMPtoCOE(char *BMPfilename, int isfinished)
     
     char bm[2];
     fseek(fp, sizeof(BITMAPFILEHEADER), 0);
-    BITMAPINFOHEADER head;
+    BITMAPINFOHEADER head;//an object of BITMAPINFOHEADER type to hold several vaules to be used
     fread(&head, sizeof(BITMAPINFOHEADER), 1, fp);
-    int picwidth = head.biWidth, picheight = head.biHeight;
-    short ind=head.biBitCount;
+    int picwidth = head.biWidth, picheight = head.biHeight;//find width and height of the picture
+    short ind=head.biBitCount;//bitcount, whether it's 24bit or 32bit
     fseek(fp, 0L, 0);
     fread(bm, 2, 1, fp);
-    //fseek(fp, 0x1CL, 0);
-    //fread(&ind, 2, 1, fp);
-    if (bm[0] != 'B' || bm[1] != 'M' )//|| ind != 32
+
+    if (bm[0] != 'B' || bm[1] != 'M' )//checks whether it's bmp
     {
         printf("this is not a bmp\n");
         return 1;
     }
     if (picheight < 0)
         picheight = -picheight;
-    fseek(fp, 0x36L, 0);
-    printf("picture dimensions:%d*%d\n", picwidth, picheight);//for checking purpose
-    int buf = (picwidth * 3 % 4) ? 4 - (picwidth * 3) % 4 : 0;
-    char *tmp = (char *)malloc(sizeof(char) * buf);
+    fseek(fp, 0x36L, 0);//0x36L == sizeof(BITMAPFILEHEADER)+sizeof(BITMAPINFOHEADER),where the actual picture starts
+    printf("picture dimensions:%d*%d\n", picwidth, picheight);//for checking purpose (I guess you should know the dimensions before using this code)
+
     int i, j;
     unsigned char r, g, b, t ;
     for (i = 0; i < picheight; i++)
         for (j = 0; j < picwidth; j++)
-        {
+        {   
+            
             fread(&b, 1, 1, fp);
             fread(&g, 1, 1, fp);
             fread(&r, 1, 1, fp);
 
-            //seperate to 4 cases and ensure that the output are always 4 bit in hexadecimal
+            //seperate to 4 cases and ensure that the output are always 4 bit in hexadecimal i.e. add zeros in front
             if ((int)(r / 256.0 * 32)*32*64+ (int)(g / 256.0 * 64)*32+ (int)(b / 256.0 * 32)>=4096)
             fprintf(outfp, "%x", (int)(r / 256.0 * 32)*32*64+ (int)(g / 256.0 * 64)*32+ (int)(b / 256.0 * 32)); 
             else if((int)(r / 256.0 * 32)*32*64+ (int)(g / 256.0 * 64)*32+ (int)(b / 256.0 * 32)>=256)
@@ -71,16 +70,23 @@ int BMPtoCOE(char *BMPfilename, int isfinished)
            I'm using it in verilog so I don't need , and ;*/
             if(ind==32) fread(&t, 1, 1, fp);//take out transparency character in 32 bit bmp
         }
-    free(tmp);
+
     fclose(fp);
     return 0;
 }
 int main()
-{
-    outfp = fopen("D:\\NUS\\EE2026\\background.coe", "w");
-    int i;
-    char s[20] = "D:\\NUS\\EE2026\\XC.bmp";
-   // fprintf(outfp, "memory_initialization_radix=16;\nmemory_initialization_vector =\n");
+{   
+    char s[20];
+    char sout[20];
+    //allows only 20 characters in the string, so pls adjust it accordingly
+    printf("please enter the path of bmp picture\n");
+    scanf("%s",s);
+   
+    printf("please enter the path of the out put file\n");
+    scanf("%s",sout);
+    outfp = fopen(sout, "w");
+    
+    // fprintf(outfp, "memory_initialization_radix=16;\nmemory_initialization_vector =\n");
     BMPtoCOE(s, 1);
     system("pause");
     return 0;
